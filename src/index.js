3da58f1eeee0535e5ec49f3c0745eee5292c96df
Nadezhda <90304907+Hopechka@users.noreply.github.com>
2022-01-25 15:12:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = '';
+    bracketsConfig.forEach((item) => {
+      for (let i of item) {
+        brackets += i;
+      }
+    });
+
+    while (str.includes(brackets)) {
+      let bracketsPlace = str.indexOf(brackets);
+      str = str.substring(0, bracketsPlace) + str.substring(bracketsPlace + 2);
+    }
+
+    return str == '' ? true : false;
 }
