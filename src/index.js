2931610cad29e10bbcd8c834b15d25e3b64effb0
Nadezhda <90304907+Hopechka@users.noreply.github.com>
2022-01-25 15:35:17 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
     let brackets = '';
-    bracketsConfig.forEach((item) => {
-      for (let i of item) {
-        brackets += i;
-      }
-    });
+  bracketsConfig.forEach((item) => {
+    for (let i of item) {
+      brackets += i;
+    }
+  });
+  let arrayBrackets = [];
+  for (let i = 0; i < brackets.length; i += 2) {
+    arrayBrackets.push(brackets[i] + brackets[i + 1]);
+  }
 
-    while (str.includes(brackets)) {
-      let bracketsPlace = str.indexOf(brackets);
-      str = str.substring(0, bracketsPlace) + str.substring(bracketsPlace + 2);
+  while (true) {
+    count = 0;
+    for (let bracketsKind of arrayBrackets) {
+      let bracketsPlace = str.indexOf(bracketsKind);
+
+      if (bracketsPlace >= 0) {
+        str =
+          str.substring(0, bracketsPlace) + str.substring(bracketsPlace + 2);
+        continue;
+      }
+
+      count++;
     }
 
-    return str == '' ? true : false;
+    if (count === arrayBrackets.length) {
+      break;
+    }
+  }
+
+  return str == '' ? true : false;
 }
